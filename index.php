6a883527ab1b6ebeadabbbd44183e9ccec67faa3
sebbekarlsson <sebastiankarlsson@Sebastians-MacBook-Air.local>
2015-03-03 09:53:06 +0100

    Navigation bar and font!
    Created a gray navigation bar and selected the font ”Roboto” for the
    website.
    I also added a search field into the navigation bar.

--- index.php
+++ index.php
@@ -2,10 +2,18 @@
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="styles/style.css">
+		<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 		<script type="text/javascript" src="apps/jquery.js"></script>
 	</head>
 
 	<body>
+		<div class="navbar">
+			<div class="navleft">
+				<ul>
+					<li><input class="intext" type="text" name="q" placeholder="search..."></li>
+				</ul>
+			</div>
+		</div>
 	</body>
 
 	<footer>
