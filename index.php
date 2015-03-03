dc8b60162bcc422392f9362d4b11df491ef349fa
sebbekarlsson <sebastiankarlsson@Sebastians-MacBook-Air.local>
2015-03-03 09:38:20 +0100

    initial commit
    added basic html code and linked the stylesheet and the jquery api

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="styles/style.css">
+		<script type="text/javascript" src="apps/jquery.js"></script>
+	</head>
+
+	<body>
+	</body>
+
+	<footer>
+	</footer>
+</html>
