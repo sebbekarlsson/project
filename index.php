05bb52c32af0cef81f07b131c038a0f5d04ecc87
sebbekarlsson <sebastiankarlsson@Sebastians-MacBook-Air.local>
2015-03-03 10:12:40 +0100

    Buttons
    setup some buttons in the navbar

--- index.php
+++ index.php
@@ -10,9 +10,24 @@
 		<div class="navbar">
 			<div class="navleft">
 				<ul>
-					<li><input class="intext" type="text" name="q" placeholder="search..."></li>
+					<li><a class="navbtn">Start</a></li>
+					<li><a class="navbtn">News</a></li>
+					<li><a class="navbtn">Projects</a></li>
+					<li><a class="navbtn">People</a></li>
 				</ul>
 			</div>
+			<div class="navright">
+				<ul>
+					<li><a class="navbtn">Sign up</a></li>
+					<li><a class="navbtn">Login</a></li>
+					<li>
+						<input class="intext" type="text" name="q" placeholder="search...">
+						<input class="btn" type="submit" value="Search">
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="content">
 		</div>
 	</body>
 
