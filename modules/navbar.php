8dac5ca69ce51997d0ba64b430317dd35d8d6f51
sebbekarlsson <sebastiankarlsson@Sebastians-MacBook-Air.local>
2015-03-04 08:25:58 +0100

    Colored buttons
    added colored buttons to the stylesheet & design

--- modules/navbar.php
+++ modules/navbar.php
@@ -1,19 +1,18 @@
 <div class="navbar">
 	<div class="navleft">
 		<ul>
-			<li><a class="navbtn" href="#">Start</a></li>
-			<li><a class="navbtn" href="#">News</a></li>
-			<li><a class="navbtn" href="#">Projects</a></li>
-			<li><a class="navbtn" href="#">People</a></li>
+			<li><a class="navbtn" href="index.php">News</a></li>
+			<li><a class="navbtn" href="projects.php">Projects</a></li>
+			<li><a class="navbtn" href="people.php">People</a></li>
 		</ul>
 	</div>
 	<div class="navright">
 		<ul>
-			<li><a class="navbtn app_pop" id="registerform">Sign up</a></li>
-			<li><a class="navbtn app_pop" id="loginform">Login</a></li>
+			<li><a class="navbtn app_pop" href="#" id="registerform">Sign up</a></li>
+			<li><a class="navbtn app_pop" href="#" id="loginform">Login</a></li>
 			<li>
 				<input class="intext" type="text" name="q" placeholder="search...">
-				<input class="btn" type="submit" value="Search">
+				<input class="btn green" type="submit" value="Search">
 			</li>
 		</ul>
 	</div>
--- modules/registerform.php
+++ modules/registerform.php
@@ -7,7 +7,7 @@
 			<input class="intext" type="text" id="reg_lastname" placeholder="lastname"><br>
 			<input class="intext" type="password" id="reg_password1" placeholder="password"><br>
 			<input class="intext" type="password" id="reg_password2" placeholder="confirm password">
-		</div>
-		<a class="btn app_close">Close</a>
+		</div><br>
+		<a class="btn app_close red" href="#">Close</a>
 	</div>
 </div>
